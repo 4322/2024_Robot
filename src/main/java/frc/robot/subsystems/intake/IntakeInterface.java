93b8889cf1a8f94f592cc6520759953601bf4402
zaydkhan@frc4322.com <zaydkhan@frc4322.com>
2024-01-21 10:37:02 -0800

    intake interface

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeInterface.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.intake;
+
+public interface IntakeInterface {
+  public void intake();
+  public void outtake();
+  public void deploy();
+  public void undeploy();
+  public void stop();
+}
