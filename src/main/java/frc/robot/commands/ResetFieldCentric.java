1f6f5d6981f06f0a05f2a4310551eeb45979768b
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-01-28 09:11:17 -0800

    Revert "refactor resetFieldCentric"
    This reverts commit d71bf544e29afc7b54f68c34f2f3bc5abc3a4bc4.

--- src/main/java/frc/robot/commands/ResetFieldCentric.java
+++ src/main/java/frc/robot/commands/ResetFieldCentric.java
@@ -24,8 +24,8 @@ public class ResetFieldCentric extends InstantCommand {
   @Override
   public void initialize() {
     if (DriverStation.isDisabled() || runWhenEnabled) {
-      driveSubsystem.resetFieldCentric();
-      String report = "Reset Field Centric";
+      driveSubsystem.resetFieldCentric(offset);
+      String report = "Reset Field Centric for " + offset + " degrees";
       DriverStation.reportWarning(report, false);
     }
   }
