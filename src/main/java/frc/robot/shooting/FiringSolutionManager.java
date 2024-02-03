a792df5191510663445f960c7f90783611feda6c
Ryan Vu <ryanvu@frc4322.com>
2024-02-03 13:34:42 -0800

    write values into json

--- src/main/java/frc/robot/shooting/FiringSolutionManager.java
+++ src/main/java/frc/robot/shooting/FiringSolutionManager.java
@@ -1,6 +1,9 @@
 package frc.robot.shooting;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import frc.utility.interpolation.GenericCalculator;
 import frc.utility.interpolation.GenericFiringSolutionManager;
@@ -8,16 +11,20 @@ import frc.utility.interpolation.GenericFiringSolutionManager;
 public class FiringSolutionManager implements GenericFiringSolutionManager<FiringSolution> {
   private final ArrayList<FiringSolution> solutions;
   private final GenericCalculator<FiringSolution> calculator;
+  private ObjectMapper objectMapper;
 
-  public FiringSolutionManager(ArrayList<FiringSolution> solutionArrayList, GenericCalculator<FiringSolution> calculator) {
+  public FiringSolutionManager(ArrayList<FiringSolution> solutionArrayList, GenericCalculator<FiringSolution> calculator, 
+                                ObjectMapper objectMapper) {
     solutions = solutionArrayList;
     this.calculator = calculator;
+    this.objectMapper = objectMapper;
     calculator.init(solutions);
   }
 
   public void addSolution(FiringSolution solution) {
     solutions.add(solution);
     calculator.whenAdded();
+    objectMapper.writeValue("target/firingSolutions", solution);
   }
 
   public FiringSolution calcSolution(double currentMag, double currentDeg) {
