6c5658c3dd33631371840fa06e24d9d87dc5bfe2
Ryan Vu <ryanvu@frc4322.com>
2024-02-02 19:25:55 -0800

    remove unused imports

--- src/main/java/frc/robot/commands/IntakeDeploy.java
+++ src/main/java/frc/robot/commands/IntakeDeploy.java
@@ -1,8 +1,6 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 
 public class IntakeDeploy extends InstantCommand {
