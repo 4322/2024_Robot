33a58e9227777d525d9a12fabc8072daab788cf5
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-03 11:27:04 -0800

    change command name to be less ambiguous

--- src/main/java/frc/robot/commands/PivotToAngle.java
+++ src/main/java/frc/robot/commands/PivotToAngle.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class PivotToSpeakerAngle extends Command {
+public class PivotToAngle extends Command {
     @Override
     public void initialize() {
         
