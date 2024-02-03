c6390733355a56a7976c9deac6b72acfbf352390
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-03 09:31:34 -0800

    create skeleton pivot shooter

---
+++ src/main/java/frc/robot/commands/PivotToSpeakerAngle.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class PivotToSpeakerAngle extends Command {
+    @Override
+    public void initialize() {
+        
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    
+}
