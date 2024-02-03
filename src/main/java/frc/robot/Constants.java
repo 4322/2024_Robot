548f19f225d5740f6142284131abf9cbf15859ab
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-03 09:53:55 -0800

    add outtake constants after losing in merge

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -231,6 +231,40 @@ public final class Constants {
     }
   }
 
+  public static final class OuttakeConstants
+  {
+    public static final int topOuttakeDeviceID = 0;
+    public static final int bottomOuttakeDeviceID = 0;
+
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kF = 0;
+
+    public static final double openLoopRampSec = 0;
+    public static final double closedLoopRampSec = 0;
+    public static final int gearRatioMotorToWheel = 0;
+    public static final double kS = 0;
+    public static final double voltPerRPS = 0;   //since we likely aren't going to adjust the speed, it's likely safe to not interpolate
+    public static final int pivotDeviceID = 0;
+
+    public static final double pivotkD = 0;
+    public static final double pivotkI = 0;
+    public static final double pivotkP = 0;
+
+    public static final double pivotClosedLoopSec = 0;
+    public static final double peakPivotVoltage = 0;
+    public static final NeutralModeValue pivotDefaultNeutralMode = NeutralModeValue.Coast;
+    public static final double defaultPivotPosition = 0;
+
+    public static final double topOuttakeRPM = 0;
+    public static final double bottomOuttakeRPM = 0;
+    public static final double outtakeToleranceRPM = 0;
+    public static final double pivotToleranceRotations = 0;
+
+    public static final double maxRPM = 0;
+  }
+
   public static final class FieldConstants {
     public static double xSpeakerPosM;
     public static double ySpeakerPosM;
@@ -263,7 +297,7 @@ public final class Constants {
 
   public static final Mode currentMode = Mode.REAL;
 
-public static final boolean outtakeEnabled = false;
+public static final boolean outtakeEnabled = true;
 
   public static enum Mode {
     /** Running on a real robot. */
