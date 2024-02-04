28a1f353a8383c2e3ee75dc93bd3b409fb94110b
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-04 15:18:16 -0800

    add skeleton for LED class

--- src/main/java/frc/robot/subsystems/LED/LED.java
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -1,33 +1,59 @@
 package frc.robot.subsystems.LED;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class LED extends SubsystemBase {
-    public LedIO io;
+  public LedIO io;
+  private ArrayList<LEDState> queuedStates = new ArrayList<LEDState>();
+  private ArrayList<LEDState> prevQueuedStates = new ArrayList<LEDState>();
 
-    public LED() {
-        switch (Constants.currentMode) {
-            case REAL:
-                io = new LedIOReal();
-                break;
+  public enum LEDState {
+    idle(1), 
+    autoNoteAcq(1), 
+    shooterReady(3), 
+    gamePieceDetected(3);
 
-            case SIM:
-                break;
-            
-            case REPLAY:
-                break;
-        }
+    public int priorityNum;
 
-        if (io == null) {
-            io = new LedIO() {};
-        }
+    LEDState(int priority) {
+      priorityNum = priority;
+    }
+  }
+
+  public LED() {
+    switch (Constants.currentMode) {
+      case REAL:
+        io = new LedIOReal();
+        break;
+
+      case SIM:
+        break;
+
+      case REPLAY:
+        break;
     }
 
-    @Override
-    public void periodic() {
-        io.rainbowAnimate(1, 0.3, 5, 0);
-        io.flashAnimate(255, 0, 0, 0.6, 5, 6);
-        
+    if (io == null) {
+      io = new LedIO() {};
     }
+  }
+
+  @Override
+  public void periodic() {
+    io.rainbowAnimate(1, 0.3, 5, 0);
+    io.flashAnimate(255, 0, 0, 0.6, 5, 6);
+    io.setLED(0, 255, 0, 11, 5);
+  }
+
+  public void determineStates() {
+    
+  }
+
+  public void prioritizeStates() {
+
+  }
+  
 }
