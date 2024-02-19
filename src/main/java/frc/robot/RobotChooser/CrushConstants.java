5d2b5681ed6397f4f24fd211504fdf168a1edf9c
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-19 14:23:32 -0800

    add todo for crush constants and add gear ratio

--- src/main/java/frc/robot/RobotChooser/CrushConstants.java
+++ src/main/java/frc/robot/RobotChooser/CrushConstants.java
@@ -5,36 +5,36 @@ import frc.utility.OrangeMath;
 
 public class CrushConstants implements RobotChooserInterface {
   // drive IDs
-  public int getFrontLeftDriveID() {
+  public int getFrontLeftDriveID() { // TODO
     return 17;
   }
 
-  public int getFrontRightDriveID() {
+  public int getFrontRightDriveID() { // TODO
     return 18;
   }
 
-  public int getBackRightDriveID() {
+  public int getBackRightDriveID() { // TODO
     return 19;
   }
 
-  public int getBackLeftDriveID() {
+  public int getBackLeftDriveID() { // TODO
     return 16;
   }
 
   // rotation IDs
-  public int getFrontLeftRotationID() {
+  public int getFrontLeftRotationID() { // TODO
     return 21;
   }
 
-  public int getFrontRightRotationID() {
+  public int getFrontRightRotationID() { // TODO
     return 15;
   }
 
-  public int getBackRightRotationID() {
+  public int getBackRightRotationID() { // TODO
     return 22;
   }
 
-  public int getBackLeftRotationID() {
+  public int getBackLeftRotationID() { // TODO
     return 20;
   }
 
@@ -57,37 +57,37 @@ public class CrushConstants implements RobotChooserInterface {
         getGearRatio());
   }
 
-  public double getMaxRotationSpeedRadPerSec() {
+  public double getMaxRotationSpeedRadPerSec() { // TODO
     return 12.2718; // physical limit of the bot
   }
 
-  public double getAutoRotatekP() {
+  public double getAutoRotatekP() { // TODO
     return 0.008;
   }
 
-  public double getAutoRotatekD() {
+  public double getAutoRotatekD() { // TODO
     return 0.0004;
   }
 
   // For tuning, graph Duty Cycle Position in the REV Hardware Client
-  public double getRotationkP() {
+  public double getRotationkP() { // TODO
     return 0.009;
   }
 
-  public double getRotationkD() {
+  public double getRotationkD() { // TODO
     return 0.0002;
   }
 
   public double getGearRatio() {
-    return 7.80; // drive gear ratio // TODO
+    return 5.90278; // L2 drive gear ratio with 16t pinion
   }
 
-  public double getDrivekSVolts() {
+  public double getDrivekSVolts() { // TODO
     return 0.182;
   }
 
   // Feed Forward parameters for Drive PID
-  public double[] getDriveffSpeedMetersPerSecThresholds() {
+  public double[] getDriveffSpeedMetersPerSecThresholds() { // TODO
     // define speed at which each voltage value will be used
     double[] feedForwardMetersPerSecThreshold = new double[4];
     // values must be in ascending order
@@ -98,7 +98,7 @@ public class CrushConstants implements RobotChooserInterface {
     return feedForwardMetersPerSecThreshold;
   }
 
-  public double[] getDriveffVoltsOverMetersPerSec() {
+  public double[] getDriveffVoltsOverMetersPerSec() { // TODO
     double[] voltsOverMetersPerSecAtSpeedThresholds = new double[4];
     voltsOverMetersPerSecAtSpeedThresholds[0] = 3.3;
     voltsOverMetersPerSecAtSpeedThresholds[1] = 3.3;
@@ -107,37 +107,37 @@ public class CrushConstants implements RobotChooserInterface {
     return voltsOverMetersPerSecAtSpeedThresholds;
   }
 
-  public double getAutoTrajectoryXYkP() {
+  public double getAutoTrajectoryXYkP() { // TODO
     return 0.1;
   }
 
-  public double getAutoTrajectoryXYkI() {
+  public double getAutoTrajectoryXYkI() { // TODO
     return 0.0;
   }
 
-  public double getAutoTrajectoryXYkD() {
+  public double getAutoTrajectoryXYkD() { // TODO
     return 0.0;
   }
 
-  public double getAutoTrajectoryRotkP() {
+  public double getAutoTrajectoryRotkP() { // TODO
     return 2.0;
   }
 
-  public double getAutoTrajectoryRotkI() {
+  public double getAutoTrajectoryRotkI() { // TODO
     return 0.0;
   }
 
-  public double getAutoTrajectoryRotkD() {
+  public double getAutoTrajectoryRotkD() { // TODO
     return 0.01;
   }
 
   @Override
-  public double getMinAutoRotateStoppedPower() {
+  public double getMinAutoRotateStoppedPower() { // TODO
     return 0.003;
   }
 
   @Override
-  public double getminAutoRotateMovingPower() {
+  public double getminAutoRotateMovingPower() { // TODO
     return 0.003;
   }
 }
