d1020bbb1a83cac64e12d2a4d6d066c71eb59ce9
Ryan Vu <ryanvu@frc4322.com>
2024-02-23 18:17:10 -0800

    Auto outtake command

---
+++ src/main/java/frc/robot/commands/SetOuttakeAdjust.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.shooting.FiringSolution;
+import frc.robot.shooting.FiringSolutionManager;
+import frc.robot.subsystems.RobotCoordinator;
+import frc.robot.subsystems.outtake.Outtake;
+import frc.utility.FiringSolutionHelper;
+
+public class SetOuttakeAdjust extends Command {
+  Outtake outtake;
+  private double flywheelSpeed;
+  private double pivotAngle;
+
+  public SetOuttakeAdjust(double flywheelSpeed, double pivotAngle) {
+    outtake = Outtake.getInstance();
+    this.flywheelSpeed = flywheelSpeed;
+    this.pivotAngle = pivotAngle;
+
+    addRequirements(outtake);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    if (RobotCoordinator.getInstance().canPivot()) {
+      outtake.outtake(flywheelSpeed);
+      outtake.pivot(pivotAngle);
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    outtake.stopOuttake();
+    outtake.stopPivot();
+  }
+}
