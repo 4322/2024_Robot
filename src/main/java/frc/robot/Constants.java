bff455425acad5f55c409859227bfecdfd305918
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-25 20:58:27 -0800

    set the pwm pins for sensors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,14 +41,14 @@ public final class Constants {
 
   public static final boolean debug = true;
 
-  public static final boolean driveEnabled = true;
+  public static final boolean driveEnabled = false;
   public static final boolean intakeEnabled = true;
   public static final boolean intakeDeployerEnabled = false;
-  public static final boolean gyroEnabled = true;
+  public static final boolean gyroEnabled = false;
   public static final boolean tunnelEnabled = true;
   public static final boolean outtakeEnabled = true;
   public static final boolean outtakePivotEnabled = false;
-  public static final boolean sensorsEnabled = false;
+  public static final boolean sensorsEnabled = true;
   public static final boolean ledEnabled = false;
   public static final boolean joysticksEnabled = false;
   public static final boolean xboxEnabled = true;
@@ -397,8 +397,8 @@ public final class Constants {
   }
 
   public static final class BeamBreakConstants {
-    public static final int tunnelBeamBreakID = 0; // TODO
-    public static final int intakeBeamBreakID = 0; // TODO
+    public static final int tunnelBeamBreakID = 6;
+    public static final int intakeBeamBreakID = 5;
 
     public static final class Logging {
       public static final String key = "Sensors/";
