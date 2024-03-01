99eb9ed8b905d1847675247862f7401d19dda4a5
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-02-29 20:09:35 -0800

    Tweak wheel rotation offsets to drive straight

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -229,9 +229,9 @@ public final class Constants {
       static {
         CANCoderOffsetRotations = new double[4];
         CANCoderOffsetRotations[WheelPosition.FRONT_RIGHT.wheelNumber] = 0.689453125;
-        CANCoderOffsetRotations[WheelPosition.FRONT_LEFT.wheelNumber] = 0.530517578125;
-        CANCoderOffsetRotations[WheelPosition.BACK_RIGHT.wheelNumber] = 0.668212890625 - 0.25;
-        CANCoderOffsetRotations[WheelPosition.BACK_LEFT.wheelNumber] = 0.462158203125 + 0.25;
+        CANCoderOffsetRotations[WheelPosition.FRONT_LEFT.wheelNumber] = 0.535517578125;
+        CANCoderOffsetRotations[WheelPosition.BACK_RIGHT.wheelNumber] = 0.705322265625 - 0.25;
+        CANCoderOffsetRotations[WheelPosition.BACK_LEFT.wheelNumber] = 0.411376953125 + 0.25;
       }
     }
 
