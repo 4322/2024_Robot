67d9ecdfff1d929efbb7d0f25d4056a40895057f
Ryan Vu <ryanvu@frc4322.com>
2024-03-02 15:37:35 -0800

    reenable shooter and shooter pivot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,8 +46,8 @@ public final class Constants {
   public static final boolean intakeDeployerEnabled = true;
   public static final boolean gyroEnabled = true;
   public static final boolean tunnelEnabled = true;
-  public static final boolean outtakeEnabled = false;
-  public static final boolean outtakePivotEnabled = false;
+  public static final boolean outtakeEnabled = true;
+  public static final boolean outtakePivotEnabled = true;
   public static final boolean sensorsEnabled = true;
   public static final boolean ledEnabled = true;
   public static final boolean joysticksEnabled = false;
