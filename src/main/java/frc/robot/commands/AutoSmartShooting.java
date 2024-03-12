1ade34f2f9b4ffb345048fb9c87139c89fbd0bee
zaydkhan4322 <zaydkhan@frc4322.com>
2024-03-12 12:48:13 -0700

    add auto smart shooting

---
+++ src/main/java/frc/robot/commands/AutoSmartShooting.java
@@ -0,0 +1,67 @@
+package frc.robot.commands.OuttakeManual;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.FiringSolutions;
+import frc.robot.commands.OuttakeManual.OuttakeManualStateMachine.OuttakeManualState;
+import frc.robot.commands.OuttakeManual.OuttakeManualStateMachine.OuttakeManualTrigger;
+import frc.robot.shooting.FiringSolution;
+import frc.robot.shooting.FiringSolutionManager;
+import frc.robot.subsystems.RobotCoordinator;
+import frc.robot.subsystems.outtake.Outtake;
+import frc.utility.FiringSolutionHelper;
+import org.littletonrobotics.junction.Logger;
+
+public class AutoSmartShooting extends Command {
+  private final Outtake outtake;
+
+  public AutoSmartShooting() {
+    outtake = Outtake.getInstance();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(outtake);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    final FiringSolution solution;
+    double botMagToSpeaker =
+        FiringSolutionHelper.getVectorToSpeaker(
+                RobotCoordinator.getInstance().getRobotXPos(),
+                RobotCoordinator.getInstance().getRobotYPos())
+            .getNorm();
+    double botAngleToSpeaker =
+        FiringSolutionHelper.getVectorToSpeaker(
+                RobotCoordinator.getInstance().getRobotXPos(),
+                RobotCoordinator.getInstance().getRobotYPos())
+            .getAngle()
+            .getDegrees();
+    solution =
+        FiringSolutionManager.getInstance().calcSolution(botMagToSpeaker, botAngleToSpeaker);
+
+    Logger.recordOutput("FiringSolutions/CalculatedShot", solution.toString());
+    Logger.recordOutput("FiringSolutions/BotPoseInput/Mag", botMagToSpeaker);
+    Logger.recordOutput("FiringSolutions/BotPoseInput/Angle", botAngleToSpeaker);
+
+    if (RobotCoordinator.getInstance().canSpinFlywheel()) {
+      outtake.outtake(solution.getFlywheelSpeed());
+    }
+
+    if (RobotCoordinator.getInstance().canPivot()) {
+      outtake.pivot(solution.getShotRotations());
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    outtake.stopOuttake();
+    outtake.stopPivot();
+  }
+}
