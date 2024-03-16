48a8785ecd3737685c4db4881750718418f7b67e
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-03-16 16:01:55 -0700

    remove unnecesary imports left from previous climber implementation

--- src/main/java/frc/robot/commands/ClimberExtend.java
+++ src/main/java/frc/robot/commands/ClimberExtend.java
@@ -1,16 +1,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.climber.Climber;
 
 public class ClimberExtend extends Command {
   private Climber climber;
-  private OperatorXboxControllerRumble xBoxRumble;
 
   public ClimberExtend() {
     climber = Climber.getInstance();
-    xBoxRumble = new OperatorXboxControllerRumble();
     addRequirements(climber);
   }
 
