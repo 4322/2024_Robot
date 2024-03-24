d2dd4e1da3de3e7698a05171ef7918ddb774403f
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-03-24 10:17:35 -0700

    fix bug in state transition of state machine and decrease tunnel volt

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -426,7 +426,7 @@ public final class Constants {
     public static final double reverseEjectVoltage = -6.0;
     public static final double rewindVoltage =
         -2.0; // just enough to pull the note off the outtake wheels
-    public static final double pushUpVoltage = 3.0;
+    public static final double pushUpVoltage = 2.5;
     public static final double peakVoltage = 6.0;
     public static final double pauseSec = 0.12; // time for tunnel to stop before rewinding
     public static final double rewindSec = 0.04;
