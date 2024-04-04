6554eaab0e56a2d6d13db4bdff180cdedba9fd60
Dhrish Karangula <dhrishkarangula@frc4322.com>
2024-04-04 12:33:22 -0500

    change enabled checks in preparation for merge in main

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,14 +41,13 @@ public final class Constants {
 
   public static final boolean driveEnabled = true;
   public static final boolean intakeEnabled = true;
-  public static final boolean intakeDeployerEnabled = false;
+  public static final boolean intakeDeployerEnabled = true;
   public static final boolean gyroEnabled = true;
   public static final boolean tunnelEnabled = true;
   public static final boolean outtakeEnabled = true;
   public static final boolean outtakePivotEnabled = true;
   public static final boolean sensorsEnabled = true;
   public static final boolean ledEnabled = true;
-  public static final boolean climberEnabled = false;
   public static final boolean joysticksEnabled = false;
   public static final boolean xboxEnabled = true;
   public static final boolean autoAcquireNoteEnabled = false;
@@ -102,7 +101,7 @@ public final class Constants {
   public static final boolean driveTuningMode = false;
   public static final boolean steeringTuningMode = false;
   public static final boolean outtakeTuningMode = false;
-  public static final boolean autoRotateDebug = true;
+  public static final boolean autoRotateDebug = false;
 
   public enum DriveDegradedMode {
     normal,
