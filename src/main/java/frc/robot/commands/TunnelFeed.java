35513069483076fb33fffe21aec7e13d6fb9d1fd
Amelia Schroeder <ameliaschroeder@frc4322.com>
2024-04-07 19:17:25 -0700

    operator controller rumbles w driver controller

--- src/main/java/frc/robot/commands/TunnelFeed.java
+++ src/main/java/frc/robot/commands/TunnelFeed.java
@@ -88,7 +88,7 @@ public class TunnelFeed extends Command {
         if (RobotCoordinator.getInstance().noteInFiringPosition()) {
           tunnel.stopTunnel();
           adjustmentTimer.restart();
-          CommandScheduler.getInstance().schedule(new XboxControllerRumble());
+          CommandScheduler.getInstance().schedule(new XboxControllerRumble(),new OperatorXboxControllerRumble());
           state = State.stoppingAtOuttake;
         } else if (abortTimer.hasElapsed(Constants.TunnelConstants.feedAbortSec)) {
           state = State.abort;  // don't fry the motor
